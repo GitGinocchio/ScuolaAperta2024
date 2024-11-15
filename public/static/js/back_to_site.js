5f26a1bce8472c08b2d4f91e7ef14775fbb3a4dd
GitGinocchio <giuliotognetto2@gmail.com>
2024-11-15 19:55:11 +0100

    back to site button fix

--- public/static/js/back_to_site.js
+++ public/static/js/back_to_site.js
@@ -1,4 +1,6 @@
-// Aggiungi un evento di click al pulsante
-document.getElementById("backToMain").addEventListener("click", function() {
-    window.location.href = "https://gitginocchio.github.io/ScuolaAperta2024/"; // Cambia con il tuo sito principale
+document.addEventListener("DOMContentLoaded", (event) => {
+    // Aggiungi un evento di click al pulsante
+    document.getElementById("backToMain").addEventListener("click", function() {
+        window.location.href = "https://gitginocchio.github.io/ScuolaAperta2024/"; // Cambia con il tuo sito principale
+    });
 });
